47b8b9cdb873171a4add510fa2cb38686692e4b3
Reece Devine <reece.devine97@gmail.com>
2018-10-04 13:28:36 -0300

    initialized gulp porperly

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,6 +1,8 @@
-var gulp require('gulp');
-
-gulp.task('hello-world', function(){
-    console.log('hello world!');
-
-});
+var gulp = require('gulp');
+    var exefile = 'some/bin/path/MyNiceProgram.exe';
+    gulp.task('build', function(){
+        gulp.src(exefile).pipe(gulp.dest('../../Binaries/'));
+    });
+    gulp.task('default', ['build'], function(){
+        gulp.watch(exefile, ['build']);
+    });
