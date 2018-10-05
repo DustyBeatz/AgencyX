2ef9603a858bde9b356491760f8c28881e9a8eac
Reece Devine <reece.devine97@gmail.com>
2018-10-05 15:24:05 -0300

    created half a lightbox in html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation()
+(()=> {
+
+const  lightbox = document.querySelector('.lightbox'),
+closeLightbox = document.querySelector('.close-lightbox');
+
+lightbox.classList.add('show-lightbox');
+
+function closeLBox() {
+    lightbox.classList.remove('show-lightbox');
+    vidPlayer.pause();
+    vidPlayer.currentTime = 0;
+  }
+
+  closeLightbox.addEventListener('click', closeLBox);
+
+
+})();
